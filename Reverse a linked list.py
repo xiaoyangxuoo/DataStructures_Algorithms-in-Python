cb1b43834c4f9c708f516eab62132f3c4537452e
Martin <tjuyangxu@gmail.com>
2020-12-06 22:22:50 -0600

    recursively reverse a LL

--- Reverse a linked list.py
+++ Reverse a linked list.py
@@ -7,3 +7,17 @@ def reverse_iterative(self):
     prev = cur 
     cur = nxt 
   self.head = prev
+
+def reverse_recursive(self):
+
+  def _reverse_recursive(cur, prev):
+    if not cur:
+      return prev
+
+    nxt = cur.next
+    cur.next = prev
+    prev = cur 
+    cur = nxt 
+    return _reverse_recursive(cur, prev)
+
+  self.head = _reverse_recursive(cur=self.head, prev=None)
